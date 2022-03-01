62426aaba5f52d431c7f05871485e2dfd0fd3669
Iñigo Taibo <itaibo@hey.com>
2022-03-01 17:44:05 +0100

    use export default

--- config/index.js
+++ config/index.js
@@ -1,7 +1,9 @@
-module.exports = {
+const Config = {
 	clientId: process.env.NEXT_PUBLIC_REDDIT_CLIENT_ID,
 	clientSecret: process.env.REDDIT_CLIENT_SECRET,
 	redirectUri: process.env.NEXT_PUBLIC_REDIRECT_URI,
 	tokenDuration: process.env.NEXT_PUBLIC_TOKEN_DURATION || 'temporary',
 	tokenScope: process.env.NEXT_PUBLIC_TOKEN_SCOPE || 'identity',
 };
+
+export default Config;
